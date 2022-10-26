13487990b33d53d7105724b9d373961d07cada89
ongun <ongun.demirag@gmail.com>
2022-10-26 23:28:02 +0300

    updated title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import "./App.css";
 import { Form } from "./components";
 
 function App() {
   return (
     <>
-      <div className="flex h-full bg-[#C4C4C4D4] bg-opacity-80">
+      <div className="flex min-h-screen bg-[#C4C4C4D4] bg-opacity-80">
         <div className="w-auto h-auto m-auto rounded">
           <Form />
         </div>
